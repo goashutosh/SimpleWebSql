dc4c02653af5e3b9369446490f7d841677888c5a
OFFICE\agoswami <agoswami@csod.com>
2021-10-23 19:17:31 +0530

    Add project files.

---
+++ SimpleWebSql.Data/MovieDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleWebSql.Data.Entity;
+
+namespace SimpleWebSql.Data
+{
+    public class MovieDbContext:DbContext
+    { public MovieDbContext(DbContextOptions<MovieDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+
+    }
+}
---
+++ WebApplication10/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SimpleWebSql.Data;
+using SimpleWebSql.Data.Entity;
+
+namespace SimpleWebSql.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private MovieDbContext _movieDbContext;
+        public Movie MovieData { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, MovieDbContext movieDbContext)
+        {
+            _logger = logger;
+            _movieDbContext = movieDbContext;
+        }
+
+        public async Task OnGet()
+        {
+
+
+            MovieData = await _movieDbContext.Movie.FirstOrDefaultAsync();
+
+           
+            
+        }
+    }
+}
