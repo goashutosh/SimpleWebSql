a9a2eb70fa18d52d5b7b159344d18da28f5caa32
OFFICE\agoswami <agoswami@csod.com>
2021-10-25 07:34:41 +0530

    this

--- WebApplication10/Pages/Index.cshtml.cs
+++ WebApplication10/Pages/Index.cshtml.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
